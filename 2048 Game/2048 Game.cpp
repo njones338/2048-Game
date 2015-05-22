5da3293634cc506dd94047415e2f29564990f59c
njones338 <njones338@gmail.com>
2015-05-22 00:17:22 -0500

    First code added
    Basic direction operations, no loop implementation

---
+++ 2048 Game/2048 Game.cpp
@@ -0,0 +1,211 @@
+// 2048 Game.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <time.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	srand(time(NULL));
+	int matrix[4][4];
+	int k;
+	int rand1, rand2;
+	//int rowSize, columnSize;
+	char answer;
+	bool check = true;
+	bool continueLoop;
+
+	std::cout << "Welcome to the game!" << std::endl << std::endl;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			matrix[i][j] = 0;
+		}
+	}
+
+	matrix[rand() % 4][rand() % 4] = 2;
+	while (check) {
+		rand1 = rand() % 4;
+		rand2 = rand() % 4;
+		if (matrix[rand1][rand2] == 0) {
+			matrix[rand1][rand2] = 2;
+			check = false;
+		}
+	}
+
+	//srand(time(NULL));
+
+	//matrix[2][3] = 2;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			std::cout << matrix[i][j] << "  ";
+		}
+		std::cout << std::endl << std::endl;
+	}
+
+	std::cout << "Enter direction (L for left, R for right, U for up, D for down): " << std::endl;
+
+	std::cin >> answer;
+
+	do {
+		continueLoop = false;
+		// random generator and switch statement goes here
+	} while (continueLoop == true);
+
+	switch (answer) {
+	case 'L':
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (matrix[i][j] != 0) {
+					k = j;
+					while (k > 0) {
+						k--;
+						if (matrix[i][k] != 0 && matrix[i][k] == matrix[i][j]) {
+							matrix[i][k] += matrix[i][j];
+							matrix[i][j] = 0;
+							k = 0;
+							continueLoop = true;
+						}
+						else if (matrix[i][k] == 0 && k == 0) {
+							matrix[i][k] = matrix[i][j];
+							matrix[i][j] = 0;
+						}
+						else if (matrix[i][k] != 0) {
+							matrix[i][k + 1] = matrix[i][j];
+							matrix[i][j] = 0;
+							k = 0;
+						}
+					}
+				}
+				else {
+					continueLoop = true;
+				}
+				//std::cout << matrix[i][j] << "  ";
+			}
+			//std::cout << std::endl << std::endl;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				std::cout << matrix[i][j] << "  ";
+			}
+			std::cout << std::endl << std::endl;
+		}
+
+		break;
+	case 'R':
+		for (int i = 3; i >= 0; i--) {
+			for (int j = 3; j >= 0; j--) {
+				if (matrix[i][j] != 0) {
+					k = j;
+					while (k < 3) {
+						k++;
+						if (matrix[i][k] != 0 && matrix[i][k] == matrix[i][j]) {
+							matrix[i][k] += matrix[i][j];
+							matrix[i][j] = 0;
+							k = 3;
+						}
+						else if (matrix[i][k] == 0 && k == 3) {
+							matrix[i][k] = matrix[i][j];
+							matrix[i][j] = 0;
+						}
+						else if (matrix[i][k] != 0) {
+							matrix[i][k - 1] = matrix[i][j];
+							matrix[i][j] = 0;
+							k = 3;
+						}
+
+					}
+				}
+				//std::cout << matrix[i][j] << "  ";
+			}
+			//std::cout << std::endl << std::endl;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				std::cout << matrix[i][j] << "  ";
+			}
+			std::cout << std::endl << std::endl;
+		}
+
+		break;
+	case 'U':
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (matrix[j][i] != 0) {
+					k = j;
+					while (k > 0) {
+						k--;
+						if (matrix[k][i] != 0 && matrix[k][i] == matrix[j][i]) {
+							matrix[k][i] += matrix[j][i];
+							matrix[j][i] = 0;
+							k = 0;
+						}
+						else if (matrix[k][i] == 0 && k == 0) {
+							matrix[k][i] = matrix[j][i];
+							matrix[j][i] = 0;
+						}
+						else if (matrix[k][i] != 0) {
+							matrix[k + 1][i] = matrix[j][i];
+							matrix[j][i] = 0;
+							k = 0;
+						}
+					}
+				}
+				//std::cout << matrix[i][j] << "  ";
+			}
+			//std::cout << std::endl << std::endl;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				std::cout << matrix[i][j] << "  ";
+			}
+			std::cout << std::endl << std::endl;
+		}
+		break;
+	case 'D':
+		for (int i = 3; i >= 0; i--) {
+			for (int j = 3; j >= 0; j--) {
+				if (matrix[j][i] != 0) {
+					k = j;
+					while (k < 3) {
+						k++;
+						if (matrix[k][i] != 0 && matrix[k][i] == matrix[j][i]) {
+							matrix[k][i] += matrix[j][i];
+							matrix[j][i] = 0;
+							k = 3;
+						}
+						else if (matrix[k][i] == 0 && k == 3) {
+							matrix[k][i] = matrix[j][i];
+							matrix[j][i] = 0;
+						}
+						else if (matrix[k][i] != 0) {
+							matrix[k - 1][i] = matrix[j][i];
+							matrix[j][i] = 0;
+							k = 3;
+						}
+					}
+				}
+				//std::cout << matrix[i][j] << "  ";
+			}
+			//std::cout << std::endl << std::endl;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				std::cout << matrix[i][j] << "  ";
+			}
+			std::cout << std::endl << std::endl;
+		}
+		break;
+	default:
+		std::cout << "Not a valid direction!" << std::endl;
+		break;
+	}
+
+	return 0;
+}
